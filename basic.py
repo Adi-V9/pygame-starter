ecd82c91e36ff46424e0ad4c861cb918f829a46b
Adi-V9 <158106363+Adi-V9@users.noreply.github.com>
2024-06-15 13:06:55 +1000

    Create basic.py
    Just a basic starter program for Pyjama projects!

---
+++ basic.py
@@ -0,0 +1,18 @@
+import sys
+import pygame
+
+# Initialising settings
+pygame.init()
+screen = pygame.display.set_mode((500, 500)) # Change size here
+pygame.display.set_caption("Pygame Basic Window") # Change caption here
+screen.fill('white') # Change colour here
+
+run = True
+
+while run:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+      sys.exit() # Optional but preferable to immediately exit
+
+  pygame.display.update()
